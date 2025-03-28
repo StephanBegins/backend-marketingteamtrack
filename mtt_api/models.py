7f0f18cd1970d2d9e8f0d69a94fb3151bb445b3a
StephanBegins <stephan1217at@gmail.com>
2025-03-28 23:49:12 +0530

    models.py udpate

--- mtt_api/models.py
+++ mtt_api/models.py
@@ -5,8 +5,8 @@ class TaskHeader(models.Model):
     user_id = models.CharField(max_length=255)  # User ID
     date = models.DateField()  # Task date
     time = models.CharField(max_length=20)  # Time in "HH:MM AM/PM" format
-    customer_type = models.CharField(max_length=50, choices=[('Existing Customer', 'Existing Customer'), ('New Customer', 'New Customer')])
-    task_type = models.CharField(max_length=50, choices=[('New Order', 'New Order'), ('Payment', 'Payment'), ('Meeting Customer', 'Meeting Customer')])
+    customer_type = models.CharField(max_length=500, choices=[('Existing Customer', 'Existing Customer'), ('New Customer', 'New Customer')])
+    task_type = models.CharField(max_length=500, choices=[('New Order', 'New Order'), ('Payment', 'Payment'), ('Meeting Customer', 'Meeting Customer')])
     no_of_customers = models.PositiveIntegerField()  # Number of customers
     remarks = models.TextField(blank=True, null=True)  # Remarks
 
